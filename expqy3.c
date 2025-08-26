2daa351809a38bc23020b85ec4481e34dca7dc03
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-08-27 00:23:15 +0530

    commit

--- expqy3.c
+++ expqy3.c
@@ -4,7 +4,7 @@ int main()
     char name[100];
     int age;
     printf("Enter your name: ");
-    scanf("%s", &name);
+    scanf("%s", name);
     printf("Enter your age: ");
     scanf("%d", &age);
     printf("%s\n", name);
