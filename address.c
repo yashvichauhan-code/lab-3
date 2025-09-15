c2ab4608654846c23d149b9114878660cbb980b7
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-15 22:30:11 +0530

    new

--- address.c
+++ address.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 int main()
 { 
-    char name[30], city[20], country[50];
-    int houseno[20];
-    //taking input
     printf("name:Yashvi Chauhan\n");
-   // scanf("%s",name);
     printf("house number: 17, Gurugram\n");
-    //scanf("%s",houseno);
     printf("city/state: Haryana\n");
-   // scanf("%s",city);
     printf("country: India\n");
-    //scanf("%s",country);
-    //output
     return 0;
 }
